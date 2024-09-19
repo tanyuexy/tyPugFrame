342b26e42d9430dd31e498b19e6351bf2d4bafe6
xy <xiaoyi@tanyue.cc>
2024-09-19 18:35:50 +0800

    取消watch config配置文件然后添加git忽略

--- watchFile.js
+++ watchFile.js
@@ -2,7 +2,7 @@ import chokidar from "chokidar";
 import fse from "fs-extra";
 import path from "path";
 import { exec } from "child_process";
-import { pathIsSame, debounce } from "./utils.js";
+import { debounce } from "./utils.js";
 import { generateGetDataFn } from "./generate.js";
 import { config } from "./config.js";
 const __dirname = path.resolve();
@@ -34,36 +34,6 @@ function watchTemplate() {
     .on("unlinkDir", refreshPagFn);
 }
 
-let fnOutput = config.fnOutput;
-let staticOutput = config.staticOutput;
-function watchConfig() {
-  let watch = chokidar.watch("./config.json", {
-    persistent: true
-  });
-  watch
-    .on("error", (error) => {
-      console.error(`Watcher error: ${error}`);
-    })
-    .on("add", () => {
-      let gitignore = fse.readFileSync("./.gitignore").toString();
-      if (!gitignore.includes(fnOutput)) {
-        gitignore += `${fnOutput}\n`;
-        fse.writeFileSync("./.gitignore", gitignore);
-      }
-      if (!gitignore.includes(staticOutput)) {
-        gitignore += `${staticOutput}\n`;
-        fse.writeFileSync("./.gitignore", gitignore);
-      }
-    })
-    .on("change", () => {
-      config = fse.readJSONSync("./config.json");
-      let gitignore = fse.readFileSync("./.gitignore").toString();
-      gitignore.replace(fnOutput, config.fnOutput);
-      gitignore.replace(staticOutput, config.staticOutput);
-    });
-}
-
 (() => {
   watchTemplate();
-  watchConfig();
 })();
